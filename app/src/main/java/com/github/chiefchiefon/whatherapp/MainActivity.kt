844c503c59b056245177a3a8df1c3cc147b3ddd4
= <shaul.chamoula@mail.huji.ac.il>
2020-12-11 11:33:30 +0200

    clear commented code after: constraints change to ref. from Top to Bottom added.

--- app/src/main/java/com/github/chiefchiefon/whatherapp/MainActivity.kt
+++ app/src/main/java/com/github/chiefchiefon/whatherapp/MainActivity.kt
@@ -18,24 +18,5 @@ class MainActivity : AppCompatActivity() {
         val adapter = WeatherDaysAdapter()
 
         weatherDAysList.adapter = adapter
-//        val weatherTV = findViewById<TextView>(R.id.weatherTV)
-//        val tempTodayTV = findViewById<TextView>(R.id.tempTodayTV)
-//        val topImage = findViewById<ImageView>(R.id.topImage)
-//
-//        val cityTV = findViewById<TextView>(R.id.cityTV)
-//
-//
-//        weatherTV.setText(R.string.weatherText)
-//        weatherTV.setTextSize(TypedValue.COMPLEX_UNIT_SP, resources.getDimension(R.dimen.today_weather_text_size))
-//
-//        tempTodayTV.setText(R.string.celsiusTempToday)
-//        tempTodayTV.setTextSize(TypedValue.COMPLEX_UNIT_SP, resources.getDimension(R.dimen.today_temp_size))
-//
-//        topImage.setImageDrawable(resources.getDrawable(R.drawable.ic_celsius_line, resources.newTheme()))
-//        //I couldn't delete the android:src="@mipmap/weather_64 of ImageView @ activity_main.xml
-//        topImage.contentDescription = R.drawable.ic_celsius_line.toString()
-//
-//        cityTV.setText(R.string.city_name)
-//        cityTV.setTextSize(TypedValue.COMPLEX_UNIT_SP, resources.getDimension(R.dimen.city_name_size))
     }
 }
